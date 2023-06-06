e9ecdda1721cb70f133cc78df5b6bfcbe71699d2
Yash Jindal <54243398+yashj069@users.noreply.github.com>
2023-06-06 19:33:12 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -8,7 +8,7 @@ export const sendCookie = (user, res, statusCode = 200, message) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      samesite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
